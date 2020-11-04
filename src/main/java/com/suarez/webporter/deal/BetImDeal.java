6bbdf23909bbd2374eae1d3368226aad4d51008f
xuefei.wang@trustway.cn <!QAZ2wsx>
2020-11-05 01:08:47 +0800

    优化客户端

---
+++ src/main/java/com/suarez/webporter/deal/BetImDeal.java
@@ -0,0 +1,177 @@
+package com.suarez.webporter.deal;
+
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.suarez.webporter.deal.bet_wb.Bet_Wb_Info;
+import com.suarez.webporter.domain.DataInfo;
+import com.suarez.webporter.domain.MatchTeam;
+import com.suarez.webporter.domain.TeamInfo;
+import com.suarez.webporter.util.Util;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Component
+public class BetImDeal extends BasicDeal {
+
+    public void begin() {
+        while (true) {
+            try {
+                Set<String> Wbkeys = redisUtil.keysByPre("im");
+                Set<String> Betkeys = redisUtil.keysByPre("bet");
+                for (String betkey : Betkeys) {
+                    float maxSimilarity = 0;
+                    String tmp_keyName = "";
+                    for (String wbkey : Wbkeys) {
+                        float tmp = Util.levenshtein(wbkey, betkey);
+                        if (tmp > maxSimilarity) {
+                            maxSimilarity = tmp;
+                            tmp_keyName = wbkey;
+                        }
+                    }
+                    if (maxSimilarity > 0.4) {
+                        //分析
+                        deal(redisUtil.get(betkey), redisUtil.get(tmp_keyName));
+                    } else {
+                        //System.out.println(betkey+"未找到匹配的赛事....");
+                    }
+                    //redisUtil.removeKey(betkey);
+                }
+
+            } catch (Exception e) {
+                //TODO
+            }
+        }
+    }
+
+    /**
+     * 计算分析
+     *
+     * @param betStr bet
+     * @param wbStr  wb
+     */
+    private void deal(String betStr, String wbStr) {
+        Gson gson = new Gson();
+        TeamInfo teamInfoBet = gson.fromJson(betStr, TeamInfo.class);
+        String keyBet = teamInfoBet.getKeyName();
+        List<DataInfo> infoListBet = teamInfoBet.getInfo();
+
+        TeamInfo teamInfoWb = gson.fromJson(wbStr, TeamInfo.class);
+        String keyWb = teamInfoWb.getKeyName();
+        List<DataInfo> infoListWb = teamInfoWb.getInfo();
+
+        //记录匹配比赛
+        String matchKey = "match:" + teamInfoBet.getKeyName() + " || " + teamInfoWb.getKeyName();
+        MatchTeam matchTeam = new MatchTeam();
+        matchTeam.setKey(matchKey);
+        matchTeam.setTeam_One(teamInfoBet.getKeyName());
+        matchTeam.setTeam_Two(teamInfoWb.getKeyName());
+        redisUtil.set(matchKey,new Gson().toJson(matchTeam));
+
+        Map<String, DataInfo> mapBet = Maps.newHashMap();
+        for (DataInfo dataInfoBet : infoListBet) {
+            String key = dataInfoBet.getPoint();
+            mapBet.put(key, dataInfoBet);
+        }
+        for (DataInfo dataInfoWb : infoListWb) {
+            String pointWb = dataInfoWb.getPoint();
+            DataInfo dataInfoBet = mapBet.get(pointWb);
+            if (dataInfoBet != null) {
+                ResultInfo resultInfo_big = WebPhaser.webpoterPhase(dealConfig.getWebpoterPhaseMoney(), keyBet,
+                        Double.valueOf(dataInfoBet.getBig_pl()),
+                        Double.valueOf(dataInfoWb.getSm_pl()), pointWb,dealConfig.getWebpoterPhaseEarnMoney());
+
+                if (resultInfo_big.getIsTrue()) {
+                    //推送消息
+                    Bet_Wb_Info info = new Bet_Wb_Info();
+                    info.setIsTrue(true);
+                    //设置bet信息
+                    info.setTeam_bet(teamInfoBet.getKeyName());
+                    info.setPk_bet(dataInfoBet.getPoint());
+                    info.setPllx_bet("大");
+                    info.setPl_bet(dataInfoBet.getBig_pl());
+                    info.setMoney_bet(resultInfo_big.getBig_money());
+                    //设置wb信息
+                    info.setTeam_wb(teamInfoWb.getKeyName());
+                    info.setPk_wb(dataInfoWb.getPoint());
+                    info.setPllx_wb("小");
+                    info.setPl_wb(dataInfoWb.getSm_pl());
+                    info.setMoney_wb(resultInfo_big.getSm_money());
+                    //设置金额
+                    info.setEnrn_money(resultInfo_big.getEnrn_money());
+                    map.put(teamInfoBet.getKeyName() + info.getPllx_bet(), info);
+                    String infoStr = "(BET)场次: " + info.getTeam_bet() + " (BET)盘口:" + info.getPl_bet() + " (BET)赔率:(" + info.getPllx_bet() + ")" + info.getPl_bet() +
+                            "(WB)场次: " + info.getTeam_wb() + " (WB)盘口:" + info.getPl_wb() + " (WB)赔率:(" + info.getPllx_wb() + ")" + info.getPl_wb() +
+                            " 盈利金额:【" + info.getEnrn_money() + "】";
+                    System.out.println(infoStr);
+                }
+
+                //bet-小 wb-大
+                ResultInfo resultInfo_sm = WebPhaser.webpoterPhase(dealConfig.getWebpoterPhaseMoney(), keyWb,
+                        Double.valueOf(dataInfoBet.getSm_pl()),
+                        Double.valueOf(dataInfoWb.getBig_pl()), pointWb,dealConfig.getWebpoterPhaseEarnMoney());
+                //TODO 推送消息
+                if (resultInfo_sm.getIsTrue()) {
+                    //推送消息
+                    Bet_Wb_Info info = new Bet_Wb_Info();
+                    info.setIsTrue(true);
+                    //设置bet信息
+                    info.setTeam_bet(teamInfoBet.getKeyName());
+                    info.setPk_bet(dataInfoBet.getPoint());
+                    info.setPllx_bet("小");
+                    info.setPl_bet(dataInfoBet.getSm_pl());
+                    info.setMoney_bet(resultInfo_sm.getSm_money());
+                    //设置wb信息
+                    info.setTeam_wb(teamInfoWb.getKeyName());
+                    info.setPk_wb(dataInfoWb.getPoint());
+                    info.setPllx_wb("大");
+                    info.setPl_wb(dataInfoWb.getBig_pl());
+                    info.setMoney_wb(resultInfo_sm.getBig_money());
+                    //设置金额
+                    info.setEnrn_money(resultInfo_sm.getEnrn_money());
+                    map.put(teamInfoBet.getKeyName() + info.getPllx_bet(), info);
+                    String infoStr = "(BET)场次: " + info.getTeam_bet() + " (BET)盘口:" + info.getPk_bet() + " (BET)赔率:(" + info.getPllx_bet() + ")" + info.getPl_bet() +
+                            "(WB)场次: " + info.getTeam_wb() + " (WB)盘口:" + info.getPl_wb() + " (WB)赔率:(" + info.getPllx_wb() + ")" + info.getPl_wb() +
+                            " 盈利金额:【" + info.getEnrn_money() + "】";
+                    System.out.println(infoStr);
+                }
+            }
+        }
+
+        //TODO 测试
+//        for (int i = 0; i < 500; i++) {
+//            Bet_Wb_Info info = new Bet_Wb_Info();
+//            info.setIsTrue(true);
+//            Random random = new Random();
+//            //设置bet信息
+//            info.setTeam_bet("5555" + i);
+//            info.setPk_bet("5555");
+//            info.setPllx_bet("大");
+//            info.setPl_bet(String.valueOf(random.nextInt()));
+//            info.setMoney_bet("5555");
+//            //设置wb信息
+//            info.setTeam_wb("0000");
+//            info.setPk_wb(String.valueOf(random.nextInt(10)));
+//            info.setPllx_wb("小");
+//            info.setPl_wb("0000");
+//            info.setMoney_wb("0000");
+//            //设置金额
+//            info.setEnrn_money("0000");
+//            map.put(info.getTeam_bet() + info.getPllx_bet(), info);
+//            String infoStr = "(BET)场次: " + info.getTeam_bet() + " (BET)盘口:" + info.getPl_bet() + " (BET)赔率:(" + info.getPllx_bet() + ")" + info.getPl_bet() +
+//                    "(WB)场次: " + info.getTeam_wb() + " (WB)盘口:" + info.getPl_wb() + " (WB)赔率:(" + info.getPllx_wb() + ")" + info.getPl_wb() +
+//                    " 盈利金额:【" + info.getEnrn_money() + "】";
+//            //System.out.println(infoStr);
+//        }
+
+    }
+
+    @Override
+    public void run() {
+        begin();
+    }
+}
---
+++ src/main/java/com/suarez/webporter/driver/ImDriver.java
@@ -0,0 +1,193 @@
+package com.suarez.webporter.driver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.util.List;
+
+@Slf4j
+@Component
+public class ImDriver {
+    @Autowired
+    private WebporterConfig webporterConfig;
+
+    private WebDriver driver;
+    private Actions action;
+    public void init(){
+        System.setProperty("webdriver.chrome.driver", webporterConfig.getWebdriverAddresses());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.setExperimentalOption("debuggerAddress", webporterConfig.getImty_listenAddress());
+        driver = new ChromeDriver(options);
+//        action = new Actions(driver);
+        WebElement frame=driver.findElement(By.xpath( "/html//iframe" ));
+
+        driver.switchTo().frame(frame);
+    }
+    public void setAttribuate(WebElement eleemnt, String attrName, String attrValue){
+
+
+        ((JavascriptExecutor)driver).executeScript("arguments[0].setAttribute(arguments[1],arguments[2])", eleemnt,attrName,attrValue);
+
+    }
+
+    public void removeAttribuate(WebElement eleemnt,String attrName,String attrValue)
+    {
+        ((JavascriptExecutor)driver).executeScript("arguments[0].removeAttribute(arguments[1],arguments[2])", eleemnt,attrName,attrValue);
+
+    }
+    public void scrollbar(WebElement element) {
+        try{
+            if(element.isEnabled()&&element.isDisplayed()){
+                ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", element);// 参数为true时调用该函数，页面（或容器）发生滚动，使element的顶部与视图（容器）顶部对齐；参数为false时，使element的底部与视图（容器）底部对齐。
+            }
+            else{
+                System.out.println("页面上的元素无法进行点击操作");
+            }
+        }catch(StaleElementReferenceException e){
+            System.out.println("页面元素没有附加在网页中");
+        }catch(NoSuchElementException e){
+            System.out.println("在页面中没有找到要操作的元素");
+        }catch(Exception e){
+            System.out.println("无法完成单机动作"+e.getStackTrace());
+        }
+    }
+
+    public void javaScriptClick(WebElement element) {
+        try{
+            if(element.isEnabled()&&element.isDisplayed()){
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click();",element);
+            }
+            else{
+                System.out.println("页面上的元素无法进行点击操作");
+            }
+        }catch(StaleElementReferenceException e){
+            System.out.println("页面元素没有附加在网页中");
+        }catch(NoSuchElementException e){
+            System.out.println("在页面中没有找到要操作的元素");
+        }catch(Exception e){
+            System.out.println("无法完成单机动作"+e.getStackTrace());
+        }
+    }
+
+    private List<WebElement> getTeamList(WebDriver driver) {
+        List<WebElement> teamList = driver.findElements(By.className("matchArea"));
+        return teamList;
+    }
+    public void focusOn(String name,String pankou,String daxiaoqiu){
+        try{
+            WebElement web = driver.findElement(By.xpath("//div[text()='"+name+"']"));
+            //scrollbar(web);
+            try{
+                if ("大".equals(daxiaoqiu)) {
+                    WebElement daqiu = driver.findElements(By.xpath("//div[text()='"+name+"']//ancestor::div[@class='event_row']//div[@class='event_even'][5]")).get(0).findElements(By.xpath("div/span")).get(0);
+
+
+                    this.javaScriptClick(daqiu);
+
+                } else if ("小".equals(daxiaoqiu)) {
+                    WebElement xiaoqiu = driver.findElements(By.xpath("//div[text()='"+name+"']//ancestor::div[@class='event_row']//div[@class='event_even'][5]")).get(0).findElements(By.xpath("div/span")).get(1);
+
+                    this.javaScriptClick(xiaoqiu);
+
+                }
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(null, "盘口已不存在.", "提示", JOptionPane.QUESTION_MESSAGE);
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "队伍已不存在.", "提示", JOptionPane.QUESTION_MESSAGE);
+        }
+
+
+//        this.javaScriptClick(xiaoqiu);
+
+//        //TODO 选中方法
+//        List<WebElement> teamList = this.getTeamList(driver);
+//        if(teamList.size()==0){
+//            JOptionPane.showMessageDialog(null, "队伍列表为空.", "提示", JOptionPane.INFORMATION_MESSAGE);
+//        }
+//        boolean flag=false;
+//        boolean pk_flag=false;
+//        boolean dxq_flag=false;
+//
+//        for (int i = 0; i < teamList.size(); i++) {
+//            WebElement team = teamList.get(i);
+//            try{
+//                if (null != team) {
+//                    //获取盘口列表
+//                    List<WebElement> teamNameDivList = team.findElements(By.className("multiOdds"));
+//                    WebElement firstPanKouElement = teamNameDivList.get(0);
+//
+//                    for(int j=0;j<teamNameDivList.size();j++){
+//                        WebElement panKouElement = teamNameDivList.get(j);
+//
+//                        //获取第一个盘口，通过第一个盘口获取队名
+//                            List<WebElement> spanElementList = firstPanKouElement.findElements(By.tagName("span"));
+//                            if(spanElementList.size()==0){
+//                                continue;
+//                            }
+//                            //主队名称
+//                            String zhudui = spanElementList.get(0).getAttribute("innerHTML").trim();
+//                            if (name.equals(zhudui)) {
+//                                flag=true;
+//                                List<WebElement> pankouList = panKouElement.findElements(By.className("odds"));
+//                                if (pankouList.size() > 1) {
+//                                    WebElement element = pankouList.get(1);
+//
+//                                    WebElement dqElement = element.findElements(By.className("betArea")).get(0);
+//                                    WebElement xqElement = element.findElements(By.className("betArea")).get(1);
+//                                    List<WebElement> dqSpanList = dqElement.findElements(By.tagName("span"));
+//
+//                                    String dqpkName = dqSpanList.get(0).getAttribute("innerHTML").trim();
+//                                    pankou = pankou.replace(".0","");
+//                                    if (dqpkName.equals(pankou)) {
+//                                        pk_flag=true;
+//                                        if ("大".equals(daxiaoqiu)) {
+//                                            dxq_flag=true;
+//                                            WebElement dqPl = dqElement.findElements(By.tagName("span")).get(1);
+//                                            this.javaScriptClick(dqPl);
+//
+//                                        } else if ("小".equals(daxiaoqiu)) {
+//                                            dxq_flag=true;
+////                                    setAttribuate(xqElement, "style", "background:#efce06;font-size:25px!important;padding:5px;color:red");
+//                                            WebElement xpPl = xqElement.findElements(By.tagName("span")).get(1);
+//                                            this.javaScriptClick(xpPl);
+//
+//                                        }
+//                                        break;
+//                                    }
+//                                }
+//                        }
+//                    }
+//
+//
+//                }
+//            }catch(Exception e){
+//
+//            }
+//
+//        }
+//        if(!flag){
+//            JOptionPane.showMessageDialog(null, "没有找到匹配的队伍.", "提示", JOptionPane.INFORMATION_MESSAGE);
+//        }else{
+//            if(!pk_flag){
+//                JOptionPane.showMessageDialog(null, "没有找到匹配的盘口.", "提示", JOptionPane.INFORMATION_MESSAGE);
+//
+//            }else{
+//
+//                if(!dxq_flag){
+//                    JOptionPane.showMessageDialog(null, "没有找到匹配的大小球.", "提示", JOptionPane.INFORMATION_MESSAGE);
+//
+//                }
+//            }
+//        }
+
+
+    }
+}
